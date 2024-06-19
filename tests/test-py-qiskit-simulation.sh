fe935631067cf1e9f1a2f37813db333a7d91b620
Stanislav Protasov <stanislav.protasov@riverlane.com>
2024-06-19 16:57:11 +0100

    fixed

--- tests/test-py-qiskit-simulation.sh
+++ tests/test-py-qiskit-simulation.sh
@@ -3,9 +3,9 @@ venv=$extension/.aqueduct-extension-dev
 
 # recreate and populate a clean virtual env
 # comment these 3 lines if requirements are stable
-# rm -r $venv
-# python -m venv $venv
-# $venv/bin/pip install -r $extension/requirements.txt
+rm -r $venv
+python -m venv $venv
+$venv/bin/pip install -r $extension/requirements.txt
 
 export aqueduct_url="https://aqueduct-demo.azurewebsites.net/"
 
